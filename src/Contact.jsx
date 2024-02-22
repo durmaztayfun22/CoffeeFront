d0f6c495d37e74ef8b2bea04da66352c82968800
lovis durmaz <durmaztayfun178@gmil.com>
2024-02-22 18:47:46 +0300

    Contact.jsx and css sections have been edited

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,24 +2,21 @@ import React from "react";
 import './Contact.css'
 
 const Contact = () => {
-  return (
-    <div className="container" style={{ width: '100%' }}>
+  return ( 
+    <main>
       <h1>Contact our page.</h1>
-      <main>
-          <div className="card" style={{ width: '100%' }}>
-            <div className="card-text">
-              <img src="https://gcdnb.pbrd.co/images/pFSaAb1xAHL2.jpg?o=1" alt="contact" />
-              <p><span>Address: 1234 Örnek Cadde, Sample Neighbourhood, Sample City, Country, 12345</span></p>
-              <p><span>Telephone: (123) 456-7890, E-mail: info@ornek.com, Contact Form: [Form]</span></p>
-
-              <p><span>Social Media: - Facebook: [Facebook], - Twitter: [Twitter], - Instagram: [Instagram]</span></p>
-              <p><span>Map [Map View]</span></p>
-              <p><span>Working Hours: Monday - Friday: 9:00 - 17:00, Saturday - Sunday: Closed</span></p>
-              <p><span>Frequently Asked Questions (FAQ): [FAQ Page]</span></p>
-            </div>
-          </div>
-      </main>
-    </div>
+      <div className="kart" style={{ width: '100%' }}>
+        <div className="kart-text">
+          <img src="https://gcdnb.pbrd.co/images/pFSaAb1xAHL2.jpg?o=1" alt="contact" className="kart-img"/>
+          <p><span>Address: 1234 Örnek Cadde, Sample Neighbourhood, Sample City, Country, 12345</span></p>
+          <p><span>Telephone: (123) 456-7890, E-mail: info@ornek.com, Contact Form: [Form]</span></p>
+          <p><span>Social Media: - Facebook: [Facebook], - Twitter: [Twitter], - Instagram: [Instagram]</span></p>
+          <p><span>Map [Map View]</span></p>
+          <p><span>Working Hours: Monday - Friday: 9:00 - 17:00, Saturday - Sunday: Closed</span></p>
+          <p><span>Frequently Asked Questions (FAQ): [FAQ Page]</span></p>
+        </div>
+      </div>
+    </main>
   );
 };
 
